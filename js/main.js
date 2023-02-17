ee1eacfdd0e301626130da000caa972a4e871062
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-02-17 17:07:03 +0100

    end

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+//* MILESTONE 1
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+//* BONUS
+//* Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      message: "ciao",
+      emails: [],
+    };
+  },
+  computed: {
+    randomEmailRequest() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            // console.log(response.data);
+            this.emails.push(response.data.response);
+          });
+      }
+    },
   },
 });
 app.mount("#root");
